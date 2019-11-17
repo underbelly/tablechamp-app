4f024a222316b1075f64e31faf3bf401219a2632
Matt Howa <matt.j.howa@gmail.com>
2019-11-17 07:56:31 -0700

    Added api info

--- js/firebase-key.js
+++ js/firebase-key.js
@@ -1,5 +1,5 @@
 var cs = { 
-  "apiKey": '', 
-  "authDomain": '', 
-  "databaseURL": '' 
+  "apiKey": 'AIzaSyC3VGqkUIfPXHFbM6e-LE4JFYUBhPwg_ZQ',
+  "authDomain": 'tablechamp-444aa.firebaseapp.com',
+  "databaseURL": 'https://tablechamp-444aa.firebaseio.com'
 };
